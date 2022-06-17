61002cf9943c1f5a20ae1c4a2c5ff96931931e65
jhoandvid <rojasalmariojhoandvid@gmail.com>
2022-06-16 20:34:59 -0500

    CRUD Arquitectura por capas

---
+++ MyMusic.Api/Validators/SaveMusicResourceValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using FluentValidation;
+using MyMusic.Api.Resources;
+using MyMusic.Core.Models;
+
+namespace MyMusic.Api.Validators
+{
+    public class SaveMusicResourceValidator : AbstractValidator<SaveMusicResource>
+    
+    {
+        public SaveMusicResourceValidator()
+        {
+            RuleFor(m => m.Name).NotEmpty().MaximumLength(50);
+            RuleFor(m => m.ArtistId).NotEmpty().WithMessage("'Artist Id' must not be o");
+        }
+    
+    }
+}
